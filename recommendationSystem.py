aef410f208fd865373a722c5c90a2cbf488b881a
Gagandeep Singh <gagansingh0797@gmail.com>
2023-10-27 01:17:48 -0400

    Update recommendationSystem.py

--- recommendationSystem.py
+++ recommendationSystem.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from fastapi import FastAPI
@@ -9,12 +8,20 @@ import pickle
 
 app = FastAPI()
 
-def embedding_query(query, model):
-    query = query.split(' ')
-    query_vec = np.zeros(100).reshape((1,100))
+
+def embedding_query(query):
+    model = Word2Vec.load("./recomModel.model")
+    print("tes", 'blue' in model.wv)
+    print("before",query)
+    query = str(query).split(" ")
+    query = [i.replace('"',"") for i in query]
+    print(query)
+    query_vec = np.zeros(100).reshape((1, 100))
     count = 0
     for word in query:
+        print(word in model.wv)
         if word in model.wv:
+            print("word", word, " ", model.wv[word])
             query_vec += model.wv[word]
             count += 1.
     if count != 0:
@@ -22,34 +29,40 @@ def embedding_query(query, model):
     return query_vec
 
 
-def get_similarity(query, n_top, df, model):
-    query_vec = embedding_query(query, model)
+def get_similarity(query, n_top, df):
+    print(query)
+    query_vec = embedding_query(query)
     # df['cleaned_embedding'] = df['cleaned_embedding'].s
     # df['cleaned_embedding'] = df['cleaned_embedding'].apply(lambda arr: [float(x) for x in arr if x not in [] ])
+    print(query_vec)
+
+    print(df['cleaned_embedding'])
     df["cos_sim"] = df['cleaned_embedding'].apply(
         lambda x: cosine_similarity(
             [x], query_vec.reshape(1, -1))[0][0])
+    print(df["cos_sim"])
     top_list = (df.sort_values("cos_sim", ascending=False)
                 [["product_id"]]
                 .drop_duplicates()[:n_top])
     return top_list
 
+
 @app.post("/recommendation/")
 def getRecommendation(text):
-    recom_model = Word2Vec.load("./recomModel.model")
-    with open("./embeddings_vector.pkl", 'rb') as file:
+    text = str(text)
+    with open("embeddings_vector.pkl", 'rb') as file:
         embedding_df = pickle.load(file)
 
-    recom_products = get_similarity(text, 10, embedding_df, recom_model)
-    total_data = pd.read_csv("./data.csv")
+    recom_products = get_similarity(text, 10, embedding_df)
+    total_data = pd.read_csv("data.csv")
     jsonResponse = []
 
     for index, row in recom_products.iterrows():
         product_id = row['product_id']
-        product = total_data[total_data["product_id"]==product_id].iloc[0]
+        product = total_data[total_data["product_id"] == product_id].iloc[0]
         jsonObj = {
             "productId": str(row['product_id']),
-            "productTitle": str(product['title']),
+            "productName": str(product['title']),
             "productPrice": str(product['variant_price']),
             "productImageUrl": str(product['images'])
         }
@@ -57,4 +70,3 @@ def getRecommendation(text):
     # json_compatible_item_data = jsonable_encoder({"data": jsonResponse})
     # print(jsonResponse)
     return JSONResponse(content={"data": jsonResponse})
-
